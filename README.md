310fe9d3662d023e21edb91a1c715731e71d53f7
Dimitris <dimitris.frangiadakis@hotmail.com>
2018-08-19 22:38:11 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# ICON(ICX) Wallet Info Telegram bot
+Telegram bot, providing information for an ICON(ICX) wallet.
+ 
+Just experimenting a bit with the ICON RPC API
+
+# Installation instructions
+**0. Prerequisites:**
+
+- Node.js >= 8.6.0 (tested)
+- Telegram
+
+**1. Create telegram bot:**
+
+- Launch [@BotFather](https://telegram.me/BotFather)
+- Type `/newBot`, give it a name and a username
+- When you're done, note down your bot's HTTP API token and username
+
+**2. Download files:**
+
+```
+cd ~
+git clone https://github.com/mitsosf/icon-telegram-bot.git
+cd icon-telegram-bot
+npm install
+cp configExample.js config.js
+```
+
+**3. Config**
+
+Now edit the config.js file and substitute the value of myToken to the telegram bot HTTP API token you received from @BotFather on the first step
+
+eg. `'telegramBotToken: '658782587:BBWjeuxwakakeoritu_NWJekstikxawjtnm',`
+
+**4. Start the script**
+
+```
+node index.js
+```
+
+**5. Test it**
+
+Go to [https://telegram.me/bot]() where `bot` is your bot's username (the one you set in @BotFather) and start a chat
+
+eg. `https://telegram.me/sampleBot`
+
+Now all you have to do, is follow the instructions provided by the bot
+
+# Future/New Features:
+Still considering, any suggestions appreciated
+
+- Add decimals to the displayed balance
+- Track multiple addresses per bot
+
+# ICX tips:
+If you found this fun or useful, please consider donating some ICX at:
+
+`hxcca2a68d4a887b40b5872aa81e6ea306be8df01d`
+
+Cheers!
+
---
+++ index.js
@@ -0,0 +1,92 @@
+const config = require('./config');
+const TeleBot = require('telebot');
+const bot = new TeleBot(config.telegramBotToken);
+const request = require('request');
+const bigInt = require('big-integer');
+
+//Global Variables
+let chatId = '0'; //Unique chat ID
+let address = 'hx0000000000000000000000000000000000000000'; //Set address for monitoring
+const timeout = 2000; //Used for aesthetic purposes
+const interval = config.interval;  //Time be
+
+//Welcome message
+bot.on('/start', (msg) => {
+    msg.reply.text('Welcome to the ICX Balance bot. This bot will let you know when your balance changes in an ICX wallet of yours');
+    setTimeout(() => {
+        msg.reply.text('Send the command "/address yourAddress" where yourAddress is your ICX address')
+    }, timeout);
+    setTimeout(() => {
+        msg.reply.text('eg. /address hxc4193cda4a75526bf50896ec242d6713bb6b02a3')
+    }, timeout);
+});
+
+//Add address
+bot.on(/^\/address (.+)$/, (msg, props) => {
+    //Get chatID
+    chatId = msg.chat.id;
+
+    //Get Address
+    let tempAddress = props.match[1];
+
+    //Validate address
+    let hx = tempAddress.substring(0, 2); //Check if address starts with hx
+
+    let hexAddress = tempAddress.substring(2); //Get actual hex address substring (without hx)
+
+    let hexOk = /^[0-9a-fA-F]{40}$/i.test(hexAddress);  //Validate if chars are hex and if length is 40
+
+    //Send messages according to validation outcome
+    if (hx !== 'hx') {
+        msg.reply.text('Your address has to start with hx, eg. hx000000...');
+    }
+    else if (!hexOk) {
+        msg.reply.text('Your address can only have hex chars (0-9, a-f, A-F) and has to be 42 chars long (including the hx in the beginning)');
+    }
+    else {
+        //Successfully set address to monitor
+        msg.reply.text('Your address is ' + tempAddress);
+        address = tempAddress;
+    }
+});
+
+bot.connect();
+
+let lastBalance = new bigInt(0);
+setInterval(function () {
+        if (chatId !== '0' && address !== 'hx0000000000000000000000000000000000000000') {
+            request.post({
+
+                    url: config.requestURL,
+                    json: true,
+                    body: {
+                        "jsonrpc": "2.0",
+                        "method": "icx_getBalance",
+                        "id": config.requestId,
+                        "params": {
+                            "address": address
+                        }
+                    }
+                }, (error, response, body) => {
+
+
+                    let cut0x = body.result.response.substring(2);
+                    let balance = new bigInt(cut0x, 16).divide(Math.pow(10, 18));
+
+                    if (balance.value === lastBalance.value) {
+                        //do nothing
+                        lastBalance = balance;
+                    }
+                    else {
+                        lastBalance.value = balance.value;
+                        request({
+                                url: 'https://api.telegram.org/bot' + config.telegramBotToken + '/sendMessage?chat_id=' + chatId + '&text=Your new balance for the address ' + address + ' is ' + balance + ' ICX'
+                            }
+                        );
+                    }
+                }
+            )
+        }
+    },
+    interval
+);
