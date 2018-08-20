fe757291d6accaeae9b8e0be92418b288c3ff475
Dimitris <dimitris.frangiadakis@hotmail.com>
2018-08-20 03:09:25 +0300

    Changed refresh time for production

--- configExample.js
+++ configExample.js
@@ -2,5 +2,5 @@ module.exports = {
     telegramBotToken: 'myToken', //Telegram bot unique token, get this from BotFather (https://telegram.me/BotFather)
     requestId: 1234,  //You can leave this one as it is
     requestURL: 'https://wallet.icon.foundation/api/v2',  //Leave this one as it is for the main-net
-    interval: 3000 //how often do you want to check for a new balance
+    interval: 30000 //how often do you want to check for a new balance
 };
